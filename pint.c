80d5a2e3f32bd228f4b4bc29f4d60a939a71db97
dt <dtvangogh@github.com>
2020-05-14 20:18:51 +0000

    push

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void pint(stack_t **head, unsigned int line_number)
 
 	if (!*head)
 	{
-		dprintf(STDERR_FILENO, "L<%u>: can't pint, stack empty\n", line_number);
+		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
 		free_list();
 		exit(EXIT_FAILURE);
 	}
