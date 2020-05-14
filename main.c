faefa8ddad005c7a6ea9cb9be4b86fbf84c658a3
dt <dtvangogh@github.com>
2020-05-14 22:39:05 +0000

    push

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	global.file = file;
